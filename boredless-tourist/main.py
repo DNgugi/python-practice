f0d0fc14fe74c79ad8f50e0e2bb1fde7558263ab
DNgugi <dngugi42@gmail.com>
2025-07-08 08:10:31 +0300

    added test objects

---
+++ boredless-tourist/main.py
@@ -0,0 +1,11 @@
+destinations = [
+    "Paris, France",
+    "Shanghai, China",
+    "Los Angeles, USA",
+    "São Paulo, Brazil",
+    "Cairo, Egypt"
+]
+
+test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+print(destinations)
