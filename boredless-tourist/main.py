6b8e36f2806e936397887c81fe3fcb95d484145a
DNgugi <dngugi42@gmail.com>
2025-07-08 08:31:54 +0300

    Created attractions and functionality for adding new attractions

--- boredless-tourist/main.py
+++ boredless-tourist/main.py
@@ -18,4 +18,23 @@ def get_traveler_location(traveler):
         traveler_destination_index = get_destination_index(traveler_destination)
         return traveler_destination_index
 
-print(get_traveler_location(test_traveller))
+attractions = [[] for i in range(len(destinations))]
+
+def add_attraction(destination, attraction):
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index]
+        attractions_for_destination.append(attraction)
+        return
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
